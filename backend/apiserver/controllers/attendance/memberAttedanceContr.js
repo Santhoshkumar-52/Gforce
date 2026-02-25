e594b8f9a4338eafb72964b7a9799ecb6c017232
Santhoshkumar52 <santhosh5002kumar@gmail.com>
2026-02-25 22:27:53 +0530

    Updating attednance & members to api server

--- backend/apiserver/controllers/attendance/memberAttedanceContr.js
+++ backend/apiserver/controllers/attendance/memberAttedanceContr.js
@@ -1,6 +1,10 @@
 import express from "express";
 import axios from "axios";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+import memberplan from "../../model/memberplan.js";
+import membermaster from "../../model/customermaster.js";
+import m_attendanceLog from "../../model/memberattendance.js";
 
 dotenv.config();
 
@@ -19,14 +23,105 @@ attedanceRouter.post("/checkin", async (req, res) => {
       });
     }
 
-    const response = await axios.post(`${DBSERVERURL}/db/attendance/checkin`, {
-      branchid,
-      memberid,
+    const { ObjectId } = mongoose.Types;
+
+    const result = await membermaster.aggregate([
+      {
+        $match: {
+          customerId: parseInt(memberid),
+          branchId: new ObjectId(branchid),
+        },
+      },
+      {
+        $lookup: {
+          from: "memberplan",
+          let: { memberId: "$_id" },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $and: [
+                    { $eq: ["$memberId", "$$memberId"] },
+                    { $eq: ["$isExpired", false] },
+                    { $eq: ["$isActive", true] },
+                  ],
+                },
+              },
+            },
+            {
+              $sort: { _id: -1 }, // latest plan
+            },
+            {
+              $limit: 1,
+            },
+          ],
+          as: "latestPlan",
+        },
+      },
+      {
+        $unwind: {
+          path: "$latestPlan",
+          preserveNullAndEmptyArrays: false, // if no plan exists
+        },
+      },
+      {
+        $addFields: {
+          "latestPlan.expiryDate": {
+            $dateToString: {
+              format: "%d-%m-%Y",
+              date: "$latestPlan.expiryDate",
+            },
+          },
+        },
+      },
+      {
+        $lookup: {
+          from: "planmaster",
+          let: { planId: "$latestPlan.planId" },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $and: [
+                    { $eq: ["$_id", "$$planId"] },
+                    { $eq: ["$is_active", true] },
+                    // add more conditions here
+                  ],
+                },
+              },
+            },
+          ],
+          as: "planDetails",
+        },
+      },
+      {
+        $unwind: {
+          path: "$planDetails",
+          preserveNullAndEmptyArrays: false, // like INNER JOIN
+        },
+      },
+    ]);
+
+    if (result.length < 1) {
+      return res.status(200).json({
+        status: "info",
+        message:
+          "Oops, No Plan Found for this Member.Try later or Please Contact Admin",
+      });
+    }
+
+    const attendanceRecord = await m_attendanceLog.create({
+      branchId: new ObjectId(branchid),
+      memberId: result[0]._id,
+      checkIn: new Date(),
+      status: "IN",
     });
+
+    console.log("Plan Found for this Member");
+
     res.status(200).json({
-      status: response.data.status,
-      message: response.data.message,
-      result: response.data.result || 0,
+      status: "ok",
+      result,
     });
   } catch (error) {
     console.error("Error in attendance route:", error);
--- backend/apiserver/controllers/member/member.js
+++ backend/apiserver/controllers/member/member.js
@@ -1,197 +1,249 @@
-import express from 'express';
-import axios from 'axios';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import Membermaster from "../../model/customermaster.js";
 
-dotenv.config()
+dotenv.config();
 
-const memberRouters = express.Router()
-const DBSERVERURL = process.env.DBSERVERURL
+const memberRouters = express.Router();
 
-memberRouters.post('/getmembers', async (req, res) => {
-    const { clientid } = req.body;
+memberRouters.post("/getmembers", async (req, res) => {
+  const { clientid } = req.body;
 
-    if (!clientid) {
-        return res.status(400).json({ message: "branchid is not specified" });
+  if (!clientid) {
+    return res.status(400).json({ message: "branchid is not specified" });
+  }
+
+  try {
+    const records =
+      clientid == "all"
+        ? await Membermaster.find({})
+        : await Membermaster.find({ branchId: clientid });
+
+    if (records.length < 1 || !records) {
+      return res.status(200).json({ message: "No records found" });
     }
-
-    try {
-        const response = await axios.post(`${DBSERVERURL}/db/membercontrol`, { clientid });
-        res.status(200).json(response.data);
-    } catch (err) {
-        console.error("Error connecting to DB Server:", err.message);
-        return res.status(502).json({
-            status: "error",
-            message: "Something went wrong while connecting to DB Server",
-            title: "Server Error"
-        });
-    }
+    console.log("Records found: ", records.length);
+    return res.status(200).json({ records });
+  } catch (err) {
+    console.error("Error connecting to DB Server:", err.message);
+    return res.status(502).json({
+      status: "error",
+      message: "Something went wrong while connecting to DB Server",
+      title: "Server Error",
+    });
+  }
 });
-memberRouters.post('/addmember', async (req, res) => {
-    let {
-        memberid,
-        firstname,
-        lastname,
-        gender,
-        dob,
-        marital_status,
-        mobile,
-        alternate_mobile,
-        email,
-        address_line1,
-        address_line2,
-        area,
-        city,
-        pincode,
-        clientid
-    } = req.body;
-    if (!gender || !dob || !mobile || !address_line1 || !area) {
-        return res.status(200).json({
-            status: "error",
-            message: "Required fields are missing",
-            title: "Validation Error"
-        })
-    }
-    if (mobile == alternate_mobile) {
-        return res.status(200).json({
-            status: "error",
-            message: "Mobile and Alternate mobile cannot be same",
-            title: "Validation Error"
-        })
-    }
-    if (memberid.length == 0) {
-        memberid = Math.floor(1000 + Math.random() * 8500);
-    }
+memberRouters.post("/addmember", async (req, res) => {
+  let {
+    memberid,
+    firstname,
+    lastname,
+    gender,
+    dob,
+    marital_status,
+    mobile,
+    alternate_mobile,
+    email,
+    address_line1,
+    address_line2,
+    area,
+    city,
+    pincode,
+    clientid,
+  } = req.body;
+  if (!gender || !dob || !mobile || !address_line1 || !area) {
+    return res.status(200).json({
+      status: "error",
+      message: "Required fields are missing",
+      title: "Validation Error",
+    });
+  }
+  if (mobile == alternate_mobile) {
+    return res.status(200).json({
+      status: "error",
+      message: "Mobile and Alternate mobile cannot be same",
+      title: "Validation Error",
+    });
+  }
+  if (memberid.length == 0) {
+    memberid = Math.floor(1000 + Math.random() * 8500);
+  }
 
-    try {
-        const response = await axios.post(`${DBSERVERURL}/db/membercontrol/addmember`, {
-            memberid,
-            firstname,
-            lastname,
-            gender,
-            dob,
-            marital_status,
-            mobile,
-            alternate_mobile,
-            email,
-            address_line1,
-            address_line2,
-            area,
-            city,
-            pincode,
-            branchid: clientid
-        });
-        const data = response.data;
-        res.status(200).json({
-            status: data.status,
-            text: data.message,
-            title: data.title
-        });
-
-    } catch (err) {
-        console.error("Error connecting to DB Server:", err.message);
-        return res.status(502).json({
-            status: "error",
-            message: "Something went wrong while connecting to DB Server",
-            title: "Server Error"
-        });
+  try {
+    const duplicateCheck = await Membermaster.findOne({
+      $or: [{ mobile: mobile }, { email: email }, { customerId: memberid }],
+    });
+    if (duplicateCheck) {
+      return res.status(200).json({
+        status: "error",
+        message: "Member Id or Mobile or Email already exists",
+        title: "Duplicate Entry",
+      });
     }
+    await Membermaster.create({
+      customerId: memberid,
+      firstname,
+      lastname,
+      gender,
+      branchId: clientid, // mongoose will cast to ObjectId
+      dob: dob ? new Date(dob) : undefined,
+      marital_status,
+      mobile,
+      alternate_mobile,
+      email,
+      address_line1,
+      address_line2,
+      area,
+      city,
+      pincode,
+    });
+    console.log("New member inserted with Id: ", memberid);
+
+    return res.status(201).json({
+      status: "success",
+      message: "Member inserted successfully",
+      title: "Success",
+    });
+  } catch (err) {
+    console.error("Error connecting to DB Server:", err.message);
+    return res.status(502).json({
+      status: "error",
+      message: "Something went wrong while connecting to DB Server",
+      title: "Server Error",
+    });
+  }
 });
-memberRouters.get('/getdetails', async (req, res) => {
-    const { memberid } = req.query;
+memberRouters.get("/getdetails", async (req, res) => {
+  let { memberid } = req.query;
 
-    if (!memberid) {
-        return res.status(400).json({ message: "Memberid is not specified" });
-    }
-    console.log("API: Getting member Details for Id:", memberid);
+  if (!memberid) {
+    return res.status(400).json({ message: "Memberid is not specified" });
+  }
+  console.log("API: Getting member Details for Id:", memberid);
 
-    try {
-        const response = await axios.get(`${DBSERVERURL}/db/membercontrol/getdetails`, { params: { memberid } });
-        if (response.status === 204) {
-            return res.status(204).json({ message: "No records found" });
-        }
-        res.status(202).json(response.data);
-    } catch (err) {
-        console.error("Error connecting to DB Server:", err.message);
-        return res.status(502).json({
-            status: "error",
-            message: "Something went wrong while connecting to DB Server",
-            title: "Server Error"
-        });
+  try {
+    memberid = Number(memberid);
+
+    const result = await Membermaster.find({
+      customerId: memberid,
+    });
+
+    if (result.length === 0) {
+      return res.status(204).json({ message: "No records found" });
     }
+
+    console.log("Member details fetched from Db for Id:", memberid);
+    return res.status(202).json({ result });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: err });
+  }
 });
-memberRouters.patch('/updatemember', async (req, res) => {
-    let {
-        memberid,
-        firstname,
-        lastname,
-        gender,
-        dob,
-        marital_status,
-        mobile,
-        alternate_mobile,
-        email,
-        address_line1,
-        address_line2,
-        area,
-        city,
-        pincode,
-        isactive
-    } = req.body;
-    const values = {
-        memberid,
-        firstname,
-        lastname,
-        gender,
-        dob,
-        marital_status,
-        mobile,
-        alternate_mobile,
-        email,
-        address_line1,
-        address_line2,
-        area,
-        city,
-        pincode,
-        isactive
-    }
+memberRouters.patch("/updatemember", async (req, res) => {
+  let {
+    memberid,
+    firstname,
+    lastname,
+    gender,
+    dob,
+    marital_status,
+    mobile,
+    alternate_mobile,
+    email,
+    address_line1,
+    address_line2,
+    area,
+    city,
+    pincode,
+    isactive,
+  } = req.body;
+  const updatedData = {
+    memberid,
+    firstname,
+    lastname,
+    gender,
+    dob,
+    marital_status,
+    mobile,
+    alternate_mobile,
+    email,
+    address_line1,
+    address_line2,
+    area,
+    city,
+    pincode,
+    isactive,
+  };
 
-    if (!gender || !dob || !mobile || !address_line1 || !area) {
-        return res.status(204).json({
-            status: "error",
-            message: "Required fields are missing",
-            title: "Validation Error"
-        })
-    }
-    if (mobile == alternate_mobile) {
-        return res.status(200).json({
-            status: "error",
-            message: "Mobile and Alternate mobile cannot be same",
-            title: "Validation Error"
-        })
-    }
-    // if (memberid.length == 0) {
-    //     memberid = Math.floor(1000 + Math.random() * 8500);
-    // }
+  if (
+    !memberid ||
+    !firstname ||
+    !lastname ||
+    !mobile ||
+    !email ||
+    !address_line1 ||
+    !city ||
+    !pincode ||
+    isactive === undefined
+  ) {
+    return res.status(204).json({
+      status: "error",
+      message: "Required fields are missing",
+      title: "Validation Error",
+    });
+  }
+  if (mobile == alternate_mobile) {
+    return res.status(200).json({
+      status: "error",
+      message: "Mobile and Alternate mobile cannot be same",
+      title: "Validation Error",
+    });
+  }
 
-    try {
-        console.log("Sending Updated Record to Db");
-
-        const response = await axios.patch(`${DBSERVERURL}/db/membercontrol/updatemember`, values);
-        const data = response.data;
-        res.status(200).json({
-            status: data.status,
-            text: data.message,
-            title: data.title
-        });
-
-    } catch (err) {
-        console.error("Error connecting to DB Server:", err.message);
-        return res.status(502).json({
-            status: "error",
-            message: "Something went wrong while connecting to DB Server",
-            title: "Server Error"
-        });
+  try {
+    const updatedData = {
+      firstname,
+      lastname,
+      gender,
+      dob,
+      marital_status,
+      mobile,
+      alternate_mobile,
+      email,
+      address_line1,
+      address_line2,
+      area,
+      city,
+      pincode,
+      isactive,
+    };
+    const result = await Membermaster.updateOne(
+      { customerId: memberid },
+      {
+        $set: updatedData,
+      },
+      { runValidators: true, new: true },
+    );
+    if (!result) {
+      return res.status(500).json({
+        status: "error",
+        message: "Failed to update member details",
+        title: "Server Error",
+      });
     }
+    return res.status(200).json({
+      status: "success",
+      message: "Member details updated successfully",
+      title: "Success",
+    });
+  } catch (err) {
+    console.error("Error connecting to DB Server:", err.message);
+    return res.status(502).json({
+      status: "error",
+      message: "Something went wrong while connecting to DB Server",
+      title: "Server Error",
+    });
+  }
 });
 
-export default memberRouters
+export default memberRouters;
